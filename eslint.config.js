c0f56087c9f8caa7ab017cc919258ffcc5ba5f0f
Duy An <duyan9646@gmail.com>
2025-09-15 20:51:08 +0700

    fix(config): fix config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,21 +4,9 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import reactPlugin from 'eslint-plugin-react';
-import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-  {
-    ignores: [
-      'dist/**',
-      'node_modules/**',
-      '*.d.ts',
-      'vite.config.ts',
-      'tailwind.config.js',
-      'postcss.config.js',
-      'src/theme/animation/',
-      'src/theme/colors.ts',
-    ],
-  },
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -27,29 +15,13 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      react: reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier: prettierPlugin,
+      react: reactPlugin,
     },
     rules: {
-      ...reactPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      ...prettierPlugin.configs.recommended.rules,
-
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-      '@typescript-eslint/no-explicit-any': 'off',
-
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'react-hooks/exhaustive-deps': 'off',
-      'prettier/prettier': 'error',
     },
     settings: {
       react: {
